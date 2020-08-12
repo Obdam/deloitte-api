4d103d706d161b0b725832eba8a51873401c3f43
Obdam <17879738+Obdam@users.noreply.github.com>
2020-08-12 11:40:07 +0200

    Created a version 1 of the POST endpoint

--- api.py
+++ api.py
@@ -4,23 +4,21 @@ from flask import request, jsonify, abort
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/home', methods=['GET'])
+@app.route('/', methods=['GET'])
 def home():
     return "<h1>Hey World</h1>"
 
-# A route to return all of the available entries in our catalog.
 @app.route('/api/companies/copyrighted', methods=['POST'])
 def add_copyright_symbol():
     if not request.json or not 'input' in request.json:
         abort(400)
+    else:
+        userinput = request.json['input']
+        companies = ['Oracle', 'Google', 'Microsoft', 'Amazon', 'Deloitte']
 
-userinput = request.json['input']
-companies = ['Oracle', 'Google', 'Microsoft', 'Amazon', 'Deloitte']
-
-for company in companies:
-    if company in userinput:
-        userinput = userinput.replace(company, company + '©')
-        # print(userinput) 
-
+        for company in companies:
+            if company in userinput:
+                userinput = userinput.replace(company, company + '©')
+                return jsonify(userinput), 201
 
 app.run()
